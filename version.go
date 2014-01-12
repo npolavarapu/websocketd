a927ecd8100d677e3cf097afc19907f4a7fc9a53
npolavarapu <npolavarapu17@lawrenceville.org>
2014-01-12 13:42:12 -0500

    Update version.go

--- version.go
+++ version.go
@@ -1,8 +1,3 @@
-// Copyright 2013 Joe Walnes and the websocketd team.
-// All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 // This value can be set for releases at build time using:
