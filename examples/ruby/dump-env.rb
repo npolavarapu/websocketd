ab24d2812895cbbc0b2c8a902fdb4d5626dbb550
npolavarapu <npolavarapu17@lawrenceville.org>
2014-01-12 14:27:18 -0500

    Update dump-env.rb

--- examples/ruby/dump-env.rb
+++ examples/ruby/dump-env.rb
@@ -1,10 +1,5 @@
 #!/usr/bin/ruby
 
-# Copyright 2013 Joe Walnes and the websocketd team.
-# All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
 # Autoflush output
 STDOUT.sync = true
 
